7742ee05cb0ec4bc868e28095ae0443f81f90ce9
Nerdycus <nerdycus@gmail.com>
2018-06-10 16:26:08 -0700

    nuu
    pls

--- Rubby/Sab.rb
+++ Rubby/Sab.rb
@@ -2,6 +2,7 @@
 
 loc = "the cottage"
 jjj = 3
+ceilfaninst=false
 coatloc="the cottage"
 rkillloc="the road"
 ceilfanloc="the backyard"
@@ -9,7 +10,17 @@ welcome = 'You are in ' + loc + '.'
 puts welcome
 	while jjj==3 do
 goose = gets
-  if goose.include? "pick up" or goose.include? "grab" or goose.include? "get" or goose.include?"obtain"
+     if goose.include?"place" or goose.include?"put" or goose.include?"install"
+   if goose.include?"ceiling fan" and goose.include?"rack"
+puts"The rack is too flimsy to hold the ceiling fan."
+   end
+   if goose.include?"ceiling fan" and (goose.include?"rack")==false and (loc=="the cottage" and (ceilfanloc=="player" or ceilfanloc==loc))
+puts"You attach the ceiling fan to the ceiling, but you'll need to wire it up before it works."
+ceilfaninst=true
+goose="drop ceiling fan"
+   end
+     end
+  if goose.include? "pick up" or goose.include? "grab" or goose.include? "get" or goose.include? "obtain"
 if goose.include?"coat" and coatloc=="player"
 puts "You're holding it!"
 elsif goose.include?"coat" and coatloc!=loc
@@ -32,7 +43,10 @@ puts "You're holding it!"
 elsif goose.include?"ceiling fan" and ceilfanloc!=loc
 puts"What fan?"
 end
-if goose.include?"fan" and ceilfanloc==loc
+if goose.include?"fan" and ceilfanloc==loc and ceilfaninst==true
+puts "It's attached, you can't remove it."
+end
+if goose.include?"fan" and ceilfanloc==loc and ceilfaninst==false
 puts "You pick up the ceiling fan."
 ceilfanloc = "player"
   if coatloc =="player"
@@ -82,8 +96,10 @@ end
 if goose.include?"ceiling fan" and ceilfanloc=="player"
 puts "You no longer have the ceiling fan."
 ceilfanloc = loc
-elsif goose.include?"ceiling fan" and ceilfanloc==loc
+elsif goose.include?"ceiling fan" and ceilfanloc==loc and ceilfaninst==false
 puts "It's already on the floor!"
+elsif goose.include?"ceiling fan" and ceilfanloc==loc and ceilfaninst == true
+puts "The fan's installed on the ceiling, you don't have it."
 elsif goose.include?"ceiling fan" and ceilfanloc!=loc
 puts"What fan?"
 end
@@ -106,6 +122,15 @@ puts "No, you decide not to."
 end
           end
           if goose.include? "look" or goose.include? "examine" or goose.include? "check"
+        if goose.include? "coat" and coatloc=="player" or coatloc==loc
+puts "A drab green coat, looks pretty old."
+        end
+        if goose.include? "ceilingfan" and (ceilfanloc=="player" or ceilfanloc==loc)
+puts"An old ceiling fan, might still work, but it's not installed anywhere."
+        end
+        if goose.include? "roadkill" and (rkillloc=="player" or rkillloc==loc)
+puts"Used to be some kind of animal. Eeeeeew."
+        end
   if goose.include? ("backyard") or goose.include?("out") or goose.include?("back")
     if loc == "the cottage" or loc == "the backyard"
 puts "It's a backyard, fenced off with rugged old sticks. It has a tiny garden, and only leads to the cottage."
@@ -125,14 +150,23 @@ puts "You can't see that!"
 if goose.include? " in" or goose.include? "cottage"
     if loc=="the backyard" or loc=="the cottage" or loc=="the road"
 puts "A small, quaint little cottage, and it leads to a backyard and the road. You expect the residents are long gone."
-if coatloc =="the cottage" and loc =="the cottage"
-puts "You see a coat hanging on a hook."
+if coatloc =="the cottage" and loc =="the cottage" and ceilfaninst==false
+puts "You see a coat hanging on a rack."
 end
-if ceilfanloc=="the cottage" and loc =="the cottage"
+if coatloc =="the cottage" and loc =="the cottage" and ceilfaninst==true
+puts "You see a coat hanging on a rack, and there's a ceiling fan on the ceiling."
+end
+if coatloc !="the cottage" and loc =="the cottage" and ceilfaninst==false
+puts "You see a coat-hanging rack."
+end
+if coatloc !="the cottage" and loc =="the cottage" and ceilfaninst==true
+puts "You see a coat-hanging rack, and a ceiling fan on the ceiling."
+end
+if ceilfanloc=="the cottage" and loc =="the cottage" and ceilfaninst == false
 puts "You see a ceiling fan on the ground."
 end
 if rkillloc=="the cottage" and loc =="the cottage"
-puts "You see a roadkill on the ground. It's bleeding on the floor."
+puts "You see a roadkill on the ground. It's bleeding all over the floor."
 end
     else
 puts "You can't see that!"
